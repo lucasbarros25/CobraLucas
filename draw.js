c81bc28567955dc309bcbbc9827732ab4e50fd80
lucasoutor <lucasoutor@gmail.com>
2020-08-17 15:32:47 -0300

    Cobra funcional

--- draw.js
+++ draw.js
@@ -3,18 +3,28 @@ const ctx = canvas.getContext("2d");
 const scale = 10;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
-
 var snake;
 
-(function setup()
-{
-		snake = new Snake();
-		
-		window.setInterval(() => {
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			snake.update();
-			snake.draw();
-		}, 250);
+(function setup() {
+  snake = new Snake();
+  fruit = new Fruit();
+  fruit.pickLocation();
+
+  window.setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    fruit.draw();
+    snake.update();
+    snake.draw();
+
+    if (snake.eat(fruit)) {
+      fruit.pickLocation();
+    }
+
+    snake.checkCollision();
+    document.querySelector('.score')
+      .innerText = snake.total;
+
+  }, 250);
 }());
 
 window.addEventListener('keydown', ((evt) => {
--- snake.js
+++ snake.js
@@ -1,25 +1,48 @@
-function Snake ()
-{
-	this.x = 0;
-	this.y = 0;
-	this.xSpeed = scale * 1;
-	this.ySpeed = 0;
- 
-	this.draw = function()
-	{
-		ctx.fillStyle = "#FFFFFF";
-		ctx.fillRect(this.x, this.y, scale, scale);
-	}
-	
-	this.update = function()
-	{
-		this.x += this.xSpeed;
-		this.y =+ this.ySpeed;
-		if (this.x > canvas.width) 
-		{
-			this.x=0;	
-		}
-	}
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xSpeed = scale * 2;
+  this.ySpeed = 0;
+  this.total = 0;
+  this.tail = [];
+
+  this.draw = function() {
+    ctx.fillStyle = "#FFFFFF";
+    for (let i=0; i<this.tail.length; i++) {
+      ctx.fillRect(this.tail[i].x,
+        this.tail[i].y, scale, scale);
+    }
+
+    ctx.fillRect(this.x, this.y, scale, scale);
+  }
+
+  this.update = function() {
+    for (let i=0; i<this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i+1];
+    }
+
+    this.tail[this.total - 1] =
+      { x: this.x, y: this.y };
+
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+
+    if (this.x > canvas.width) {
+      this.x = 0;
+    }
+
+    if (this.y > canvas.height) {
+      this.y = 0;
+    }
+
+    if (this.x < 0) {
+      this.x = canvas.width;
+    }
+
+    if (this.y < 0) {
+      this.y = canvas.height;
+    }
+  }
 	this.changeDirection = function (direction)
 	{
 		switch(direction)
@@ -49,7 +72,24 @@ function Snake ()
 			this.ySpeed = 0;
 			break;
 		}
-
-
 	}
+this.eat = function(fruit) {
+    if (this.x === fruit.x &&
+      this.y === fruit.y) {
+      this.total++;
+      return true;
+    }
+
+    return false;
+  }
+
+  this.checkCollision = function() {
+    for (var i=0; i<this.tail.length; i++) {
+      if (this.x === this.tail[i].x &&
+        this.y === this.tail[i].y) {
+        this.total = 0;
+        this.tail = [];
+      }
+    }
+  }
 }
